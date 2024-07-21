b822ef3115678724aae65367d6166de735bb63d9
Nafeesa71 <nafeesafathimaa@gmail.com>
2024-07-21 21:24:22 +0530

    new file created

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+let currentResult =0
+
+currentResult = (currentResult + 10 ) * 3 + 2 -1
+
+let calculationDescrption = (0 + 10) * 3 + 2 - 1
+
+  var resultname = document.getElementById("current-result").innerText = 'calculationDescription'
+  console.log(resultname)
