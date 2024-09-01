7c59af79734ea43436ca6697bdc7b595afd79c4b
Nafeesa71 <nafeesafathimaa@gmail.com>
2024-09-02 01:10:28 +0530

    Commiting all files

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,34 @@
-let currentResult =0
+const backdropElement = document.getElementById('backdrop');
+const modalLinkElements = document.querySelectorAll('.info-modal');
+let infoModal;
 
-currentResult = (currentResult + 10 ) * 3 + 2 -1
+function toggleBackdrop() {
+  backdropElement.classList.toggle('visible');
+}
 
-let calculationDescrption = (0 + 10) * 3 + 2 - 1
+function presentInfoModal(event) {
+  const text = event.target.dataset.text;
+  toggleBackdrop();
+  infoModal = document.createElement('div');
+  infoModal.classList.add('modal');
+  infoModal.innerHTML = `
+    <h2>More Details</h2>
+    <p>${text}</p>
+  `;
+  const closeButton = document.createElement('button');
+  closeButton.addEventListener('click', hideInfoModal);
+  closeButton.textContent = 'Okay';
+  infoModal.appendChild(closeButton);
+  document.body.appendChild(infoModal);
+}
 
-  var resultname = document.getElementById("current-result").innerText = 'calculationDescription'
-  console.log(resultname)
+function hideInfoModal() {
+  toggleBackdrop();
+  document.body.removeChild(infoModal);
+}
+
+for (const linkElement of modalLinkElements) {
+  linkElement.addEventListener('click', presentInfoModal);
+}
+
+backdropElement.addEventListener('click', hideInfoModal);
